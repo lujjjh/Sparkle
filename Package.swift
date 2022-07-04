e1dfbda9be581c2572d6e7e38c8ea0b08ce71c9a
Jiahao Lu <lujjjh@gmail.com>
2022-07-04 14:52:22 +0800

    Update Package management files for version 2.0.3

--- Package.swift
+++ Package.swift
@@ -2,10 +2,10 @@
 import PackageDescription
 
 // Version is technically not required here, SPM doesn't check
-let version = "2.0.2"
+let version = "2.0.0"
 // Tag is required to point towards the right asset. SPM requires the tag to follow semantic versioning to be able to resolve it.
-let tag = "2.0.2"
-let checksum = "a545518bb5221ac025c694baba3aa3b2fb236dd217c8a01ae3d2be15fea68042"
+let tag = "2.0.3"
+let checksum = "02ae60d8b21c68cf4002ed9101fb5ce07fb920fcba1cd278ab2f0ec40e5e57cc"
 let url = "https://github.com/lujjjh/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
 
 let package = Package(
